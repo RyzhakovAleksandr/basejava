9b6e8816902ae722857a54f7fe656f830dd4e24a
Александр Рыжаков <rjizhakov@yandex.ru>
2026-02-03 11:29:50 +0500

    feat: organizing resume storage
    - added methods for storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,63 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        size = 0;
     }
 
     void save(Resume r) {
+        if (size < storage.length) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
+        int index = findResume(uuid);
+        if (index >= 0) {
+            return storage[index];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = findResume(uuid);
+        if (index != -1) {
+            if (index == size - 1) {
+                size--;
+            } else {
+                for (int i = index; i <= size - 1; i++) {
+                    storage[i] = storage[i + 1];
+                }
+                size--;
+            }
+        }
+    }
+
+    private int findResume(String uuid) {
+        int result = -1;
+        if (uuid != null) {
+            for (int i = 0; i < size; i++) {
+                if (uuid.equals(storage[i].uuid)) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] getStorage = new Resume[size];
+        if (size > 0) System.arraycopy(storage, 0, getStorage, 0, size);
+        return getStorage;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
