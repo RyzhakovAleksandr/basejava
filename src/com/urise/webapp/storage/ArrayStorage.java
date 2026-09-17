8c5e76874f9eb71bccf92ae4ba8870beca56dab9
Александр Рыжаков <rjizhakov@yandex.ru>
2026-09-17 16:43:52 +0500

    feat: hw_2 added new function UPDATE and message in methods SAVE, DELETE, GET when not find Resume

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,9 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -6,28 +12,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[SIZE_STORAGE];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        int index = findResume(r.getUuid());
+        if (index != -1) {
+            System.out.printf("Невозможно сохранить %s. Резюме уже существует%n", r.getUuid());
+            return;
+        }
         if (size < storage.length) {
             storage[size++] = r;
+        } else {
+            System.out.println("Хранилище переполнено");
         }
     }
 
-    Resume get(String uuid) {
+    public void update(Resume r) {
+        int index = findResume(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        } else {
+            System.out.printf("Невозможно обновить %s. Резюме не найдено%n", r.getUuid());
+        }
+    }
+
+    public Resume get(String uuid) {
         int index = findResume(uuid);
         if (index >= 0) {
             return storage[index];
+        } else {
+            System.out.printf("Невозможно вывести %s. Резюме не найдено%n", uuid);
         }
+
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = findResume(uuid);
         if (index != -1) {
             if (index == size - 1) {
+                storage[index] = null;
                 size--;
             } else {
                 for (int i = index; i <= size - 1; i++) {
@@ -35,6 +62,8 @@ public class ArrayStorage {
                 }
                 size--;
             }
+        } else {
+            System.out.printf("Невозможно удалить %s. Резюме не найдено%n", uuid);
         }
     }
 
@@ -42,7 +71,7 @@ public class ArrayStorage {
         int result = -1;
         if (uuid != null) {
             for (int i = 0; i < size; i++) {
-                if (uuid.equals(storage[i].uuid)) {
+                if (uuid.equals(storage[i].getUuid())) {
                     result = i;
                     break;
                 }
@@ -54,13 +83,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] getStorage = new Resume[size];
         if (size > 0) System.arraycopy(storage, 0, getStorage, 0, size);
         return getStorage;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
